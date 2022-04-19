d942793ed8b2c82dd8d049c9c25b9c813842c43c
CyanoFresh <cyanofresh@gmail.com>
2022-04-19 21:08:26 +0300

    updated deps + fixes

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -22,7 +22,7 @@ export function Welcome() {
         <Typography variant="body1" sx={{ mt: 3, mb: 4 }}>
           Turn on Bluetooth and connect a new device
         </Typography>
-        <Button onClick={() => dispatch(addDevice())} variant="round">
+        <Button onClick={() => dispatch(addDevice())} variant="round" size="large">
           Connect
         </Button>
       </div>
